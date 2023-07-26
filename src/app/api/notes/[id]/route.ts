1bf11c2c8c6ec54a3eb3f738ac93e08a33a6e8da
lvanegasimagine <lvanegas1429@gmail.com>
2023-07-26 17:41:24 -0600

    Endpoint Note Done

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -1,13 +1,70 @@
 import { NextResponse } from "next/server";
+import { Prisma } from '@prisma/client';
+import { prisma } from '@/libs/prisma'
 
-export function GET() {
-    return NextResponse.json({ message: 'Obteniendo una nota' })
+interface Params { params: { id: string } }
+
+export async function GET(request: Request, { params }: Params) {
+    try {
+        const { id } = params;
+
+        const note = await prisma.note.findFirst({
+            where: {
+                id: Number(id)
+            }
+        })
+
+        if (!note) return NextResponse.json({ message: 'Nota no encontrada' }, { status: 404 })
+        return NextResponse.json(note, { status: 200 })
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ message: error.message }, { status: 500 })
+        }
+    }
 }
 
-export function PUT() {
-    return NextResponse.json({ message: 'Actualizando una nota' })
+export async function PUT(request: Request, { params }: Params) {
+    try {
+        const { id } = params
+        const { title, content } = await request.json();
+        const updatedNote = await prisma.note.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                title,
+                content
+            }
+        })
+
+        if (!updatedNote) return NextResponse.json({ message: 'Nota no encontrada' }, { status: 404 })
+
+        return NextResponse.json(updatedNote, { status: 200 })
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') return NextResponse.json({ message: 'Nota no encontrada para actualizar' }, { status: 500 })
+            return NextResponse.json({ message: error.message }, { status: 500 })
+        }
+    }
 }
 
-export function DELETE() {
-    return NextResponse.json({ message: 'Eliminando una nota' })
+export async function DELETE(request: Request, { params }: Params) {
+    try {
+        const { id } = params
+
+        const deletedNote = await prisma.note.delete({
+            where: {
+                id: Number(id)
+            }
+        })
+
+        if (!deletedNote) return NextResponse.json({ message: 'Nota no encontrada' }, { status: 404 })
+
+        return NextResponse.json(deletedNote, { status: 200 })
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025') return NextResponse.json({ message: 'Nota no encontrada para eliminar' }, { status: 500 })
+            return NextResponse.json({ message: error.message }, { status: 500 })
+        }
+    }
 }
